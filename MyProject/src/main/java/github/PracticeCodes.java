1e8f9f55c74152b32d65fa74c4999c5c9df8f168
Alquama98 <alquamahashimi@gmail.com>
2024-11-15 13:46:49 +0530

    Source Code Push

---
+++ MyProject/src/main/java/github/PracticeCodes.java
@@ -0,0 +1,8 @@
+package github;
+
+public class PracticeCodes {
+	
+	public static void main(String[] args) {
+		System.out.println("Hi Github. We've learnt to use you as a repository for our codes");
+	}
+}
